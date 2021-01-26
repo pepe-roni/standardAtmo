f0ee9e0960abc22f28a917bc79d55b2caae16d07
Derrick Ng <ahde2k@gmail.com>
2021-01-25 16:25:19 -0800

    Add files via upload

--- standardAtmo.m
+++ standardAtmo.m
@@ -14,6 +14,23 @@ dViscosity = rawdata.DynVisc;
 %identify closest values
 disp('Calculate standard atmosphere values between -5000 to 250000ft')
 requestAlt = input('What altitude(ft) is of interest: ');
+
+%error handling for range, comment out if you are editting .csv to extend
+%range
+if requestAlt > 250000 || requestAlt < -5000
+    error('Out of range: -5000 to 250000ft')
+end
+
+if requestAlt <= 36000
+    region = "Troposphere";
+elseif requestAlt <= 167000
+    region = "Stratosphere";
+elseif requestAlt <= 232000
+    region = "Mesosphere";
+else
+    region = "Ionosphere";
+end
+
 for i=1:numel(altitude)
     difference(i,1) = abs(altitude(i) - requestAlt);
 end
@@ -30,12 +47,13 @@ outAirPressure = interpo(altitude1, altitude2, requestAlt, airPressure(closestIn
 outAirDensity = interpo(altitude1, altitude2, requestAlt, airDensity(closestInd), airDensity(closestInd2));
 outViscocity = interpo(altitude1, altitude2, requestAlt, dViscosity(closestInd), dViscosity(closestInd2));
 
-fprintf('At altitude %f ft\n', requestAlt)
-fprintf('\nAtmospheric temperature is %f F\n', outTemp)
-fprintf('Local gravity is %f ft/s^2\n', outLocalg)
-fprintf('Air pressure is %f lbs/in^2\n', outAirPressure)
-fprintf('Air density is %f*10^-4 slugs/ft^3\n', outAirDensity)
-fprintf('Dynamic viscosity is %f*10^-7 lb*s/ft^2\n', outViscocity)
+fprintf('At altitude %.3f ft', requestAlt)
+fprintf(': %s\n', region);
+fprintf('\nAtmospheric temperature is %.3f F\n', outTemp)
+fprintf('Local gravity is %.3f ft/s^2\n', outLocalg)
+fprintf('Air pressure is %.4f lbs/in^2\n', outAirPressure)
+fprintf('Air density is %.4f*10^-4 slugs/ft^3\n', outAirDensity)
+fprintf('Dynamic viscosity is %.3f*10^-7 lb*s/ft^2\n', outViscocity)
 
 function interpValue = interpo(x1,x2,x3,y1,y2)
     interpValue = (((x2-x3)*y1) + ((x3-x1)*y2))/(x2-x1);
